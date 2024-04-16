231a81bc65fb069d09bb0c3395adc589d9acfff5
Joshua Hickman <jvh985@gmail.com>
2024-04-16 16:35:49 -0500

    getting familiar with toBigDecimal and added first README file

---
+++ README.md
@@ -0,0 +1,28 @@
+# Learning Kotlin and Intellij IDE
+
+This project will be focused on getting used to the syntax of Kotlin and familiarizing
+myself with the Intellij IDE w/ git
+
+## If you are having trouble with Intellij defaulting to 'Master' Branch instead of 'Main'
+
+Initialize git in the file directory of the project:
+- start a new project but do not check the create repository option
+- when you are ready to load your progress to GitHub, navigate to project folder in terminal
+>git init
+>
+>git add . 
+>
+>git commit -m "your commit message"
+> 
+>git branch -M main
+> 
+> git remote add origin 'your repository url'
+> 
+> git push -u origin main
+
+After this your 'Main' branch will show in Intellij version control
+
+---
+
+# Project 1
+
--- src/main.kt
+++ src/main.kt
@@ -1,23 +1,47 @@
+import java.math.BigDecimal
 
+/*  Keyboard shortcuts:
+    ⌃ ␣ :brings up completion items
+    ⌥ ⏎ :brings up intentions/context actions
+    ⇧shift x2 :brings up search everywhere - includes IDE actions
+    ⌥ ↑ :to start selecting words, then lines, then blocks
+    ⌥ ↓ :reverse of above
+    ⌘ / :comments out full line of code, again will undo
+    ⌥ ⌘ / :will create block comments
+    ⌥ ⇧shift ↑ or ↓ :will move lines of code up or down
+    ⇧shift ⌘ ↑ or ↓ :will move entire functions up or down through the code
+    ⌘ - or = will collapse / expand code region
+    ⌥ ⌘ T :will surround selected code to add template code ie: try/catch
+    ⌃ G and ⌃ ⌘ G :will select the symbol at caret / select all symbols with the same name
 
+*/
 // Getting used to the syntax of Kotlin
 
+
 fun main() {
-    println("Hello World")
+    println("hello world")
 
     val name = "random person"
     val clubs = 10
     var teams = 10  // mutable type
+    val total = 25.60.toBigDecimal()
 
-    // clubs ++  Error: val is read-only
+    val shipping: BigDecimal
 
-    teams += 4   // teams == 14
-    teams ++    // teams == 15
 
-    println("Number of teams: $teams")
-    println("Number of clubs: $clubs")
+    // clubs ++  error: val is read-only
 
-    println("Hello $name")
+    teams += 4  // teams == 14
+    teams -= 2  // teams == 12
+    teams *= 2  // teams == 24
+    teams /= 3  // teams == 8
+    teams ++    // teams == 9
+    teams --    // teams == 8
+
+    println("number of teams: $teams")
+    println("number of clubs: $clubs")
+
+    println("hello $name")
 
     val condition = true
 
@@ -29,11 +53,26 @@ fun main() {
     }
 
     if (!condition) {
-        println("Condition set to false")
+        println("condition set to false")
     }
     else{
-        println("Condition set to true")
+        println("condition set to true")
     }
+
+   shipping = calculateShipping(total)
+
+    println("Shipping cost is: $${shipping}")
+
 }
 
+fun calculateShipping(total:BigDecimal): BigDecimal {
+    val shippingCost:BigDecimal
+
+    if(total < 35.0.toBigDecimal()) {
+        shippingCost = 5.00.toBigDecimal()
+    }
+    else shippingCost = 0.00.toBigDecimal()
+
+    return shippingCost
+}
 
