558bf200aeca37ca8be9af1e639cdd996e84def9
Joshua Hickman <jvh985@gmail.com>
2024-04-16 15:16:15 -0500

    Simple coding, checking syntax and IDE abilities

--- src/main.kt
+++ src/main.kt
@@ -1,19 +1,38 @@
+
+
 // Getting used to the syntax of Kotlin
 
 fun main() {
     println("Hello World")
 
     val name = "random person"
+    val clubs = 10
+    var teams = 10  // mutable type
+
+    // clubs ++  Error: val is read-only
+
+    teams += 4   // teams == 14
+    teams ++    // teams == 15
+
+    println("Number of teams: $teams")
+    println("Number of clubs: $clubs")
 
     println("Hello $name")
 
     val condition = true
 
     if (condition) {
-        println("correct")
+        println("test for condition bool type")
     }
     else {
         println("error")
+    }
+
+    if (!condition) {
+        println("Condition set to false")
+    }
+    else{
+        println("Condition set to true")
     }
 }
 
